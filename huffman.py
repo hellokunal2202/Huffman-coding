639451f9c5e6fb393bcb8dc6b4754045083e9669
Kunal Mali <malikunal2202@gmail.com>
2024-05-22 20:28:20 +0530

    Compression completed

--- huffman.py
+++ huffman.py
@@ -40,29 +40,63 @@ class HuffmanCoding:
         for key in frequency:
             node = HeapNode(key,frequency[key])
             heapq.heappush(self.heap,node)
-            
-
         
-
     def merge_codes(self):
         #build huffman tree. Save root node in heap
-        pass
+        while(len(self.heap)>1):
+            node1 = heapq.heappop(self.heap)
+            node2 = heapq.heappop(self.heap)
+
+            merged = HeapNode(None,node1.freq + node2.freq)
+            merged.left = node1
+            merged.right = node2
+
+            heapq.heappush(self.heap,merged)
+
+    def make_codes_helper(self,node,current_code):
+        #make codes for each character
+        if node == None:
+            return
+
+        if node.char != None:
+            self.codes[node.char] = current_code
+            return
+
+        self.make_codes_helper(node.left,current_code + "0")
+        self.make_codes_helper(node.right,current_code + "1")
 
     def make_codes(self):
-        #make codes for each character ans save
-        pass
+        #make codes for each character ans save 0010110
+        root = heapq.heappop(self.heap)
+        current_code = ""
+        self.make_codes_helper(root,current_code)
+        
 
     def get_encoded_text(self,text):
         #return encoded text
-        pass
+        encoded_text = ""
+        for character in text:
+            encoded_text += self.codes[character]
+
+        return encoded_text
+        
 
     def pad_encoded_text(self,encoded_text):
         #pad encoded text
-        pass
+        extra_padding = 8 - len(encoded_text) % 8
+        for i in range(extra_padding):
+            encoded_text += "0"
+        
+        padded_info = "{0:08b}".format(extra_padding)
+        encoded_text = padded_info + encoded_text
 
     def get_byte_array(self,padded_encoded_text):
         #return byte array 8 bits to byte
-        pass
+        b = bytearray()
+        for i in range(0,len(padded_encoded_text),8):
+            byte = padded_encoded_text[i:i+8]
+            b.append(int(byte,2))
+        return b
 
     def compress(self):
         filename, file_extension = os.path.splitext(self.path)
